341714b2643c8ddcc75bac6294bea8347076ac1c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-10-30 12:47:29 +0100

    Add podman::run define

--- spec/acceptance/podman_spec.rb
+++ spec/acceptance/podman_spec.rb
@@ -12,4 +12,20 @@ describe 'podman class' do
       idempotent_apply(pp)
     end
   end
+
+  context 'when running a container' do
+    let(:pp) do
+      <<-MANIFEST
+        class { 'podman': }
+        podman::run { 'redis_server':
+          image => 'docker.io/redis',
+        }
+      MANIFEST
+    end
+
+    it 'applies the manifest twice with no stderr' do
+      pending "I couldn't find a way to run podman in docker yet..."
+      idempotent_apply(pp)
+    end
+  end
 end
