b9e4e3c7b5f54fcb75e849cba47ce919d1f2a0e0
Philippe Burgisser <pburgisser@wrk89.wrk.lsn.camptocamp.com>
2019-12-24 06:55:03 +0100

    Changed variable name

--- manifests/run.pp
+++ manifests/run.pp
@@ -5,29 +5,60 @@
 # @example
 #   podman::run { 'namevar': }
 define podman::run (
-  String[1]        $image,
-  Optional[String] $user                = 'root',
-  Optional[String] $group               = 'root',
-  Optional[String] $command             = undef,
-  Optional[String] $container_run_flags = undef,
-  Array            $ports               = [],
+  Optional[String] $image                 = undef,
+  String[1]        $container_tag         = 'latest',
+  String[1]        $user                  = 'root',
+  String[1]        $group                 = 'root',
+  Optional[String] $command               = undef,
+  String           $environment_file      = "/etc/sysconfig/${name}",
+  Boolean          $manage_environment_file = true,
+  Array            $ports                 = [],
+  Array            $volumes               = [],
+  Optional[String] $container_extra_flags = undef,
+  Optional[String] $podman_extra_args    = undef,
 ) {
   include ::podman
+
+  if ($user != 'root') {
+    $service_enable = undef  # Puppet's module service doesn't support of systemd user service
+    $service_active = undef
+    $systemd_unit_file_path = "/home/${user}/.config/systemd/user"
+  }else{
+    $service_enable = true
+    $service_active = true
+    $systemd_unit_file_path = '/etc/systemd/system'
+  }
+
   systemd::unit_file { "${name}-container.service":
+    path    => $systemd_unit_file_path,
     content => epp(
       'podman/container.service.epp',
       {
-        'description'         => sprintf('%s container', capitalize($name)),
-        'image'               => $image,
-        'user'                => $user,
-        'group'               => $group,
-        'sanitised_title'     => regsubst($title, '[^0-9A-Za-z.\-_]', '-', 'G'),
-        'ports'               => $ports,
-        'command'             => $command,
-        'container_run_flags' => $container_run_flags,
+        'description'      => sprintf('%s container', capitalize($name)),
+        'sanitised_title'  => regsubst($title, '[^0-9A-Za-z.\-_]', '-', 'G'),
+        'environment_file' => $environment_file,
       }
     ),
-    enable  => true,
-    active  => true,
+    owner   => $user,
+    group   => $group,
+    enable  => $service_enable,
+    active  => $service_active,
+  }
+  file { $environment_file:
+    ensure  => file,
+    path    => $environment_file,
+    owner   => $user,
+    group   => $group,
+    content => epp(
+      'podman/sysconfig.epp',
+      {
+        'image'                 => $image,
+        'container_tag'         => $container_tag,
+        'volumes'               => $volumes,
+        'container_extra_flags' => $container_extra_flags,
+        'podman_extra_args'     => $podman_extra_args,
+      }
+    ),
+    replace => $manage_environment_file,
   }
 }
